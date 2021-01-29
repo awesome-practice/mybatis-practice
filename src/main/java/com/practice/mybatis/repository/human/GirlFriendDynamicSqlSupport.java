7ae65a83e0e271fdc5e9c2b4be5e883f0e63ee0f
LeonGeroge <leongeroge@gmail.com>
2021-01-29 23:06:30 +0800

    clean

--- src/main/java/com/practice/mybatis/repository/human/GirlFriendDynamicSqlSupport.java
+++ src/main/java/com/practice/mybatis/repository/human/GirlFriendDynamicSqlSupport.java
@@ -2,29 +2,23 @@ package com.practice.mybatis.repository.human;
 
 import java.sql.JDBCType;
 import javax.annotation.Generated;
+
 import org.mybatis.dynamic.sql.SqlColumn;
 import org.mybatis.dynamic.sql.SqlTable;
 
 public final class GirlFriendDynamicSqlSupport {
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.486+08:00", comments="Source Table: girl_friend")
     public static final GirlFriend girlFriend = new GirlFriend();
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.487+08:00", comments="Source field: girl_friend.id")
     public static final SqlColumn<Long> id = girlFriend.id;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.487+08:00", comments="Source field: girl_friend.name")
     public static final SqlColumn<String> name = girlFriend.name;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.487+08:00", comments="Source field: girl_friend.character")
     public static final SqlColumn<String> character = girlFriend.character;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.487+08:00", comments="Source field: girl_friend.thought")
     public static final SqlColumn<String> thought = girlFriend.thought;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.487+08:00", comments="Source field: girl_friend.appearance")
     public static final SqlColumn<String> appearance = girlFriend.appearance;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.487+08:00", comments="Source Table: girl_friend")
     public static final class GirlFriend extends SqlTable {
         public final SqlColumn<Long> id = column("id", JDBCType.BIGINT);
 
--- src/main/java/com/practice/mybatis/repository/human/GirlFriendMapper.java
+++ src/main/java/com/practice/mybatis/repository/human/GirlFriendMapper.java
@@ -4,8 +4,10 @@ import static com.practice.mybatis.repository.human.GirlFriendDynamicSqlSupport.
 import static org.mybatis.dynamic.sql.SqlBuilder.*;
 
 import com.practice.mybatis.repository.human.entity.GirlFriend;
+
 import java.util.List;
 import javax.annotation.Generated;
+
 import org.apache.ibatis.annotations.DeleteProvider;
 import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Mapper;
@@ -32,50 +34,41 @@ import org.mybatis.dynamic.sql.util.SqlProviderAdapter;
 
 @Mapper
 public interface GirlFriendMapper {
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.488+08:00", comments="Source Table: girl_friend")
-    @SelectProvider(type=SqlProviderAdapter.class, method="select")
+    @SelectProvider(type = SqlProviderAdapter.class, method = "select")
     long count(SelectStatementProvider selectStatement);
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.489+08:00", comments="Source Table: girl_friend")
-    @DeleteProvider(type=SqlProviderAdapter.class, method="delete")
+    @DeleteProvider(type = SqlProviderAdapter.class, method = "delete")
     int delete(DeleteStatementProvider deleteStatement);
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.489+08:00", comments="Source Table: girl_friend")
-    @InsertProvider(type=SqlProviderAdapter.class, method="insert")
+    @InsertProvider(type = SqlProviderAdapter.class, method = "insert")
     int insert(InsertStatementProvider<GirlFriend> insertStatement);
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.49+08:00", comments="Source Table: girl_friend")
-    @SelectProvider(type=SqlProviderAdapter.class, method="select")
+    @SelectProvider(type = SqlProviderAdapter.class, method = "select")
     @ResultMap("GirlFriendResult")
     GirlFriend selectOne(SelectStatementProvider selectStatement);
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.49+08:00", comments="Source Table: girl_friend")
-    @SelectProvider(type=SqlProviderAdapter.class, method="select")
-    @Results(id="GirlFriendResult", value = {
-        @Result(column="id", property="id", jdbcType=JdbcType.BIGINT, id=true),
-        @Result(column="name", property="name", jdbcType=JdbcType.VARCHAR),
-        @Result(column="character", property="character", jdbcType=JdbcType.VARCHAR),
-        @Result(column="thought", property="thought", jdbcType=JdbcType.VARCHAR),
-        @Result(column="appearance", property="appearance", jdbcType=JdbcType.VARCHAR)
+    @SelectProvider(type = SqlProviderAdapter.class, method = "select")
+    @Results(id = "GirlFriendResult", value = {
+            @Result(column = "id", property = "id", jdbcType = JdbcType.BIGINT, id = true),
+            @Result(column = "name", property = "name", jdbcType = JdbcType.VARCHAR),
+            @Result(column = "character", property = "character", jdbcType = JdbcType.VARCHAR),
+            @Result(column = "thought", property = "thought", jdbcType = JdbcType.VARCHAR),
+            @Result(column = "appearance", property = "appearance", jdbcType = JdbcType.VARCHAR)
     })
     List<GirlFriend> selectMany(SelectStatementProvider selectStatement);
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.491+08:00", comments="Source Table: girl_friend")
-    @UpdateProvider(type=SqlProviderAdapter.class, method="update")
+    @UpdateProvider(type = SqlProviderAdapter.class, method = "update")
     int update(UpdateStatementProvider updateStatement);
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.492+08:00", comments="Source Table: girl_friend")
     default QueryExpressionDSL<MyBatis3SelectModelAdapter<Long>> countByExample() {
         return SelectDSL.selectWithMapper(this::count, SqlBuilder.count())
                 .from(girlFriend);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.492+08:00", comments="Source Table: girl_friend")
     default DeleteDSL<MyBatis3DeleteModelAdapter<Integer>> deleteByExample() {
         return DeleteDSL.deleteFromWithMapper(this::delete, girlFriend);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.493+08:00", comments="Source Table: girl_friend")
     default int deleteByPrimaryKey(Long id_) {
         return DeleteDSL.deleteFromWithMapper(this::delete, girlFriend)
                 .where(id, isEqualTo(id_))
@@ -83,7 +76,6 @@ public interface GirlFriendMapper {
                 .execute();
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.493+08:00", comments="Source Table: girl_friend")
     default int insert(GirlFriend record) {
         return insert(SqlBuilder.insert(record)
                 .into(girlFriend)
@@ -96,7 +88,6 @@ public interface GirlFriendMapper {
                 .render(RenderingStrategy.MYBATIS3));
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.494+08:00", comments="Source Table: girl_friend")
     default int insertSelective(GirlFriend record) {
         return insert(SqlBuilder.insert(record)
                 .into(girlFriend)
@@ -109,19 +100,16 @@ public interface GirlFriendMapper {
                 .render(RenderingStrategy.MYBATIS3));
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.494+08:00", comments="Source Table: girl_friend")
     default QueryExpressionDSL<MyBatis3SelectModelAdapter<List<GirlFriend>>> selectByExample() {
         return SelectDSL.selectWithMapper(this::selectMany, id, name, character, thought, appearance)
                 .from(girlFriend);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.495+08:00", comments="Source Table: girl_friend")
     default QueryExpressionDSL<MyBatis3SelectModelAdapter<List<GirlFriend>>> selectDistinctByExample() {
         return SelectDSL.selectDistinctWithMapper(this::selectMany, id, name, character, thought, appearance)
                 .from(girlFriend);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.495+08:00", comments="Source Table: girl_friend")
     default GirlFriend selectByPrimaryKey(Long id_) {
         return SelectDSL.selectWithMapper(this::selectOne, id, name, character, thought, appearance)
                 .from(girlFriend)
@@ -130,7 +118,6 @@ public interface GirlFriendMapper {
                 .execute();
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.496+08:00", comments="Source Table: girl_friend")
     default UpdateDSL<MyBatis3UpdateModelAdapter<Integer>> updateByExample(GirlFriend record) {
         return UpdateDSL.updateWithMapper(this::update, girlFriend)
                 .set(id).equalTo(record::getId)
@@ -140,7 +127,6 @@ public interface GirlFriendMapper {
                 .set(appearance).equalTo(record::getAppearance);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.496+08:00", comments="Source Table: girl_friend")
     default UpdateDSL<MyBatis3UpdateModelAdapter<Integer>> updateByExampleSelective(GirlFriend record) {
         return UpdateDSL.updateWithMapper(this::update, girlFriend)
                 .set(id).equalToWhenPresent(record::getId)
@@ -150,7 +136,6 @@ public interface GirlFriendMapper {
                 .set(appearance).equalToWhenPresent(record::getAppearance);
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.497+08:00", comments="Source Table: girl_friend")
     default int updateByPrimaryKey(GirlFriend record) {
         return UpdateDSL.updateWithMapper(this::update, girlFriend)
                 .set(name).equalTo(record::getName)
@@ -162,7 +147,6 @@ public interface GirlFriendMapper {
                 .execute();
     }
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2018-09-03T09:37:35.497+08:00", comments="Source Table: girl_friend")
     default int updateByPrimaryKeySelective(GirlFriend record) {
         return UpdateDSL.updateWithMapper(this::update, girlFriend)
                 .set(name).equalToWhenPresent(record::getName)
